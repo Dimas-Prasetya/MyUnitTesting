c8c10400b72364f1f6c57fe5c5ffe7dee3318a99
Dimas Prasetya <dimas.prasetya@meteor.id>
2019-11-19 16:35:19 +0700

    first commit

---
+++ app/src/main/java/com/dimas/myunittesting/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.dimas.myunittesting
+
+class MainViewModel(private val cuboidModel: CuboidModel) {
+    fun getCircumference(): Double = cuboidModel.getCircumference()
+
+    fun getSurfaceArea(): Double = cuboidModel.getSurfaceArea()
+
+    fun getVolume(): Double = cuboidModel.getVolume()
+
+    fun save(w: Double, l: Double, h: Double) {
+        cuboidModel.save(w, l, h)
+    }
+}
